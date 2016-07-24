454ea5a5e26fcd8496a84907a4369659f4890917
Artur <artur.prado@gmail.com>
2016-07-24 19:46:49 -0300

    Change root controller to home_controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,19 @@
-class WordsController < ApplicationController
+class WordsController
   def index
+
   end
+
+  private
+
+  def lang
+    search_params[:lang] || SpreadsheetLanguage::ENGLISH
+  end
+
+  def search_params
+    params.permit(
+      :lang,
+      :q
+    )
+  end
+
 end
